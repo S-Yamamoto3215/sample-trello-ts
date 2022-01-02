a5af6208fb60e984fa13226e210006ddcc18cead
S-Yamamoto3215 <yamamotoshoya8@gmail.com>
2022-01-02 16:54:47 +0900

    react-beautiful-dnd template, basic css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import './index.css';
+
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 const App = () => {
   return (
-    <div>
+    <div className='dragDropArea'>
+      <DragDropContext onDragEnd={() => console.log(1)}>
+        <Droppable droppableId='droppable'>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Draggable draggableId='item0' index={0}>
+                {(provided) => (
+                  <div
+                    className='item'
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    item0
+                  </div>
+                )}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
